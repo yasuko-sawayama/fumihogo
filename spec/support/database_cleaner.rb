a5392d570971b45381e392a25987d5e350716304
shibuyamio <mio.shibuya@trictrac.co.jp>
2018-01-05 14:30:28 +0900

    Annotate Devise 設定

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,32 @@
+DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    unless driver_shares_db_connection_with_specs
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+    Capybara.reset_sessions!
+  end
+end
---
+++ spec/support/devise.rb
@@ -0,0 +1,9 @@
+OmniAuth.config.test_mode = true
+
+RSpec.configure do |config|
+  config.include Warden::Test::Helpers
+
+  config.after :each do
+    Warden.test_reset!
+  end
+end
