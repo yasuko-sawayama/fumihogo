25669db55b1195662312745ecdafd9f89202cc81
shibuyamio <mio.shibuya@trictrac.co.jp>
2018-03-27 13:11:30 +0900

    add redux-devtools

--- app/javascript/bundles/Product/store/productStore.js
+++ app/javascript/bundles/Product/store/productStore.js
@@ -1,8 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import ProductReducer from '../reducers/productReducer';
 
+// placeholder
+const middleware = {};
+
 const configureStore = railsProps => (
-  createStore(ProductReducer, railsProps)
-);
+  createStore(
+    ProductReducer,
+    railsProps,
+    composeWithDevTools(applyMiddleware(...middleware)),
+  ));
 
 export default configureStore;
