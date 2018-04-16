81eb839d8cf960d20916386ac0d503560082bb44
shibuyamio <mio.shibuya@trictrac.co.jp>
2018-04-16 14:16:14 +0900

    ページ作成後リダイレクト

--- app/javascript/bundles/Product/sagas/pagePostSaga.js
+++ app/javascript/bundles/Product/sagas/pagePostSaga.js
@@ -1,11 +1,10 @@
-import { takeEvery, call, put } from 'redux-saga/effects';
+import { takeEvery, call, put, all, fork } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
 
 import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 import { postEntities } from '../../../shared/libs/requestsManager';
 import { postPageSuccess, postPageError } from '../actions/postPageActionCreators';
-import { redirectToRoot } from '../../../shared/libs/redirects';
 
 import { PAGE_POST_REQUESTED } from '../constants/productConstants';
 import { PRODUCT_API_ENTRY_POINT } from '../../shared/constants/commonConstants';
@@ -16,13 +15,13 @@ export function* postPage({ payload, }) {
 //    yield put(clearError());
     const url = `${PRODUCT_API_ENTRY_POINT}${payload.productId}/pages/`;
     const response = yield call(postEntities, url, payload.data);
-    yield put(postPageSuccess(response));
-    console.log(response);
-  } catch (error) {
+    console.log(response)
     yield [
-      put(postPageError(error)),
-      // put(redirectToRoot()),
+      put(postPageSuccess(response)),
+      put(push(`/${response.data.product_id}/pages/${response.data.position}`)) // friendry_id使っているのでpositionを使う
     ]
+  } catch (error) {
+    yield put(postPageError(error));
   } finally {
     yield put(hideLoading());
   }
--- app/javascript/shared/libs/redirects.js
+++ app/javascript/shared/libs/redirects.js
@@ -1,3 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+
 const ROOT_PATH = location.protocol + '//' + location.host;
 
-export const redirectToRoot = () => location.href=ROOT_PATH
+export const redirectToRoot = () => location.href=ROOT_PATH;
+
+
+
