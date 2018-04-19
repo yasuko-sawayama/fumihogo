97e440d57d38dd33622913ea03e03d024cfc04f2
shibuyamio <mio.shibuya@trictrac.co.jp>
2018-04-19 21:06:07 +0900

    change to remote browser

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,10 +1,20 @@
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome,
-                                      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-                                        chrome_options: {
-                                          args: %w[headless disable-gpu window-size=1680,1050]
-                                        }
-                                      ))
+    if ENV['SELENIUM_DRIVER_URL'].present?
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        url: ENV.fetch('SELENIUM_DRIVER_URL'),
+        desired_capabilities: :chrome
+      )
+    else
+      Capybara::Selenium::Driver.new(
+        app, browser: :chrome,
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+          chrome_options: {
+            args: %w[headless disable-gpu window-size=1680,1050]
+          }
+        ))
+    end
 end
 
 Capybara.javascript_driver = :chrome
