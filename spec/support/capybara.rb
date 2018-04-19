ce1ab2386520b87a07027218f42ddbdd8dbb2944
shibuyamio <mio.shibuya@trictrac.co.jp>
2018-04-19 21:39:17 +0900

    remoteからinstallに戻す

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,20 +1,10 @@
 Capybara.register_driver :chrome do |app|
-    if ENV['SELENIUM_DRIVER_URL'].present?
-      Capybara::Selenium::Driver.new(
-        app,
-        browser: :remote,
-        url: ENV.fetch('SELENIUM_DRIVER_URL'),
-        desired_capabilities: :chrome
-      )
-    else
-      Capybara::Selenium::Driver.new(
-        app, browser: :chrome,
-        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-          chrome_options: {
-            args: %w[headless disable-gpu window-size=1680,1050]
-          }
-        ))
-    end
+  Capybara::Selenium::Driver.new(app, browser: :chrome,
+                                      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+                                        chrome_options: {
+                                          args: %w[headless disable-gpu window-size=1680,1050]
+                                        }
+                                      ))
 end
 
 Capybara.javascript_driver = :chrome
