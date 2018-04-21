3f45422b5c843d4f10dcf13576aeb7ee026ce0ef
shibuyamio <mio.shibuya@trictrac.co.jp>
2018-04-21 22:37:54 +0900

    Webpack production設定

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,35 @@
 const environment = require('./environment')
+const webpack = require("webpack");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = environment.toWebpackConfig()
+//config
+const config = {
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Production'
+    }),
+    new UglifyJSPlugin({
+      // sourceMap: true,
+      uglifyOptions: {
+        ecma: 6,
+        warnings: false,
+        compress: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
+    new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
+    new webpack.optimize.DedupePlugin()
+    new BundleAnalyzerPlugin(),
+  ],
+}
+
+environment.config.merge(config);
+module.exports = environment.toWebpackConfig();
