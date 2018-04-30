e650a04c12f2dfdb7adeca85a7b710bab870ea0c
shibuyamio <mio.shibuya@trictrac.co.jp>
2018-04-30 19:10:48 +0900

    設定忘れ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Setup the mailer config
+  config.action_mailer.default_url_options = { host: 'fumihogo.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
