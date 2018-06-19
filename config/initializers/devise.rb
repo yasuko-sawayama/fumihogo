a27ba40e71c5131d6416f4be2fbcb2dae37c58b8
sawayama yasuko <yasuko.sawayama@gmail.com>
2018-06-20 02:45:28 +0900

    fix path

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -253,12 +253,12 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   callback_url = if Rails.env.production?
                    if ENV['HEROKU_ENV'] == 'staging'
-                     'https://fumihogo-staging.herokuapp.com/users/omniauth_callbacks'
+                     'https://fumihogo-staging.herokuapp.com/users/auth/twitter/callback'
                    else
-                     'https://fumihogo.herokuapp.com/users/omniauth_callbacks'
+                     'https://fumihogo.herokuapp.com/users/auth/twitter/callback'
                    end
                  else
-                    'http://localhost:3000/users/omniauth_callbacks'
+                    'http://localhost:3000/users/auth/twitter/callback'
                  end
   config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'],
                   {
