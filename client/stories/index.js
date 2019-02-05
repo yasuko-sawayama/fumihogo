3732aa5e4b6dd0cdec50418212e5efdcc1e258db
sawayama yasuko <yasuko.sawayama@gmail.com>
2019-02-06 03:00:07 +0900

    storyにSCSS追加

--- client/stories/index.js
+++ client/stories/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 
+import "../app/assets/styles/test.scss"
+
 storiesOf('Button', module)
   .add('with text', () => (
     <Button>Hello Button</Button>
