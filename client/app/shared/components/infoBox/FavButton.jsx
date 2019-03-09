6e6bec791653ccd90e4a134900dfca958bfc381e
shibuyamio <mio.shibuya@trictrac.co.jp>
2019-03-09 18:10:51 +0900

    お気に入りボタン動作設定

--- client/app/shared/components/infoBox/FavButton.jsx
+++ client/app/shared/components/infoBox/FavButton.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const FavButton = props => {
+const FavButton = ({ count, faved, handleClick }) => {
+  const current = faved ? "favorite" : "favorite_border";
+  const toggle = current === "favorite" ? "favorite_border" : "favorite";
+
   return (
     <div className="favorite">
-      <i className="md-icon">favorite_border</i>
-      <button>
-        <i className="md-icon">favorite</i>
+      <i className="md-icon">{current}</i>
+      <button onClick={handleClick}>
+        <i className="md-icon">{toggle}</i>
       </button>
-      <span>24</span>
+      <span>{count}</span>
     </div>
   );
 };
 
-FavButton.propTypes = {};
+FavButton.propTypes = {
+  count: PropTypes.number,
+  faved: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired
+};
+
+FavButton.defaultProps = {
+  count: 0
+};
 
 export default FavButton;
--- client/app/shared/components/infoBox/stories/infobox.stories.jsx
+++ client/app/shared/components/infoBox/stories/infobox.stories.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf, addDecorator } from "@storybook/react";
 
-import { withKnobs } from "@storybook/addon-knobs/react";
+import { withKnobs, boolean, number } from "@storybook/addon-knobs/react";
 import { withViewport } from "@storybook/addon-viewport";
 import FavButton from "../FavButton";
 
@@ -10,4 +10,10 @@ addDecorator(withViewport("iphone6"));
 storiesOf("作品パネル", module)
   .addDecorator(withKnobs)
   .addDecorator(withViewport())
-  .add("お気に入りボタン", () => <FavButton/>);
+  .add("お気に入りボタン", () => (
+    <FavButton
+      count={number("お気に入り数", 0)}
+      faved={boolean("お気に入り", true)}
+      handleClick={() => alert("faved!")}
+    />
+  ));
